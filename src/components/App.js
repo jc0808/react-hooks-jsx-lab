41edc45650b9085671f84628ccf5b252f5a5585b
Joshua Campos <jdeveloper1203@gmail.com>
2022-09-20 14:02:46 -0400

    commit tree 45c086cd3841452349ec9e9cbb0f056fda27ed7a

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import userEvent from "@testing-library/user-event";
 
 function App() {
   return (
